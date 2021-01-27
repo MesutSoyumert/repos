749ead14e8ea5e67f2329b75a179f765764130a0
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-01-27 20:08:07 +0300

    27 0cak sonu

--- Kampintro/GenericsIntro/GenericsIntro/MyList.cs
+++ Kampintro/GenericsIntro/GenericsIntro/MyList.cs
@@ -19,11 +19,20 @@ namespace GenericsIntro
             items = new T[items.Length+1];
             for (int i = 0; i < tempArray.Length; i++)
             {
-                items[i] = tempArray[1];
+                items[i] = tempArray[i];
             }
 
             items[items.Length - 1] = item;
 
+        }
+        public int Length 
+        {
+            get { return items.Length; }        
+        }
+
+        public T[] Items
+        {
+            get { return items; }
         }
     }
 }
