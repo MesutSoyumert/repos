9c5967db0f1ec527b65d8a223188330eb50db905
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-01-27 19:51:33 +0300

    27 ocak

--- Kampintro/GenericsIntro/GenericsIntro/MyList.cs
+++ Kampintro/GenericsIntro/GenericsIntro/MyList.cs
@@ -6,35 +6,24 @@ namespace GenericsIntro
 {
     class MyList<T>
     {
-        // BU class generic class örneğidir
-        // Gelen T'nin ne olduğu önemli değildir
-        T[]items;
-        // aşağıdaki yapıya constructor denir
-        // ctor ile yazılır
-        // gelen array parametreyi new'le ve sıfır elemanlı yarat
-        // constructor otomatik olarak MyList adını aldı
+        T[] items;
+        // ctor tab tab ile yaratılır, constructor denir
+        // new yapınca sıfır elemanlı array oluştu
         public MyList()
         {
             items = new T[0];
         }
-        // GELEN T Tipinde items arrayi oluşur
-        // items değişkenine class içinde heryerden ulaşılabilir
         public void Add(T item)
         {
-            // array'in eleman sayısını bir arttır
-            // items.Length array'in eleman sayısını verir
-
             T[] tempArray = items;
-            // aşağıda items'i new'layacağımız için önceki items
-            // array'ini tempArray array'ine taşıyoruz
             items = new T[items.Length+1];
             for (int i = 0; i < tempArray.Length; i++)
             {
-                items[i] = tempArray[i];
+                items[i] = tempArray[1];
             }
 
-            items[items.Length-1] = item;
-        }
+            items[items.Length - 1] = item;
 
+        }
     }
 }
--- Kampintro/GenericsIntro/GenericsIntro/Program.cs
+++ Kampintro/GenericsIntro/GenericsIntro/Program.cs
@@ -6,10 +6,10 @@ namespace GenericsIntro
     {
         static void Main(string[] args)
         {
+
             MyList<string> isimler = new MyList<string>();
             isimler.Add("Engin");
 
-                        
             Console.WriteLine("Hello World!");
         }
     }
