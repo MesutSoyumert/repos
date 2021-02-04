0924bf3bc1d482df24eba56ea0c6b2587184876e
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-02-05 00:32:15 +0300

    4 şubat sonu

---
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // İş kuralları
+            // Yetkisi var mı?
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryID(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+    }
+}
