f8786dc5a3c61c2635d76599d9c644713e958354
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-02-12 14:14:58 +0300

    Gün 4 Ödev 5

--- KampIntro_Odevler/Gun_03_Odev_03_ClassMetotDemo/Entities/Concrete/Musteri.cs
+++ KampIntro_Odevler/Gun_03_Odev_03_ClassMetotDemo/Entities/Concrete/Musteri.cs
@@ -7,6 +7,18 @@ namespace Entities.Concrete
 {
     public class Musteri:IEntity
     {
+        public Musteri()
+        {
+
+        }
+        public Musteri(int musteriid, string musteriadisoyadi, string musterisadresi, string musterisehir)
+        {
+            MusteriId = musteriid;
+            MusteriAdiSoyadi = musteriadisoyadi;
+            MusteriAdresi = musterisadresi;
+            MusteriSehir = musterisehir;
+        }
+
         public int MusteriId { get; set; }
         public string MusteriAdiSoyadi { get; set; }
         public string MusteriAdresi { get; set; }
---
+++ KampIntro_Odevler/Gun_04_Odev_05/Gun_04_Odev_05/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gun_04_Odev_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "bir");
+            myDictionary.Add(2, "iki");
+            myDictionary.Add(3, "üç");
+            myDictionary.Add(4, "dört");
+            myDictionary.Add(5, "beş");
+            myDictionary.Add(6, "altı");
+            myDictionary.Add(7, "yedi");
+            myDictionary.Add(8, "sekiz");
+            myDictionary.Add(9, "dokuz");
+
+            myDictionary.Key(6);
+        }
+    }
+}
+
+
+class MyDictionary<TKey, TValue>
+{
+    List<TKey> _keys;
+    List<TValue> _values;
+    public MyDictionary()
+    {
+        _keys = new List<TKey>();
+        _values = new List<TValue>();
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        _keys.Add(key);
+        _values.Add(value);
+    }
+
+    public void Key(TKey key)
+    {
+        Console.WriteLine(_values[_keys.IndexOf(key)]);
+    }
+
+}
